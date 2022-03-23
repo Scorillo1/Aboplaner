6ca14685345ccd5117fd05a6809782720faed297
Valentin <Valentin@DESKTOP-QBNE14B>
2022-03-23 09:15:40 +0100

    test

---
+++ src/aboplaner/Aboplaner.java
@@ -0,0 +1,28 @@
+package aboplaner;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+// @author Valentin, Julian, Sebastian
+
+public class Aboplaner extends Application
+{
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
